b0b696f14a1095a74253f10bd889e53d6ab2f92a
Thomas <quentin.sebring@icloud.com>
2021-03-29 18:36:13 -0500

    Started Menu and colors

---
+++ Assets/Scripts/Deuteranomaly.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Instance.setColor1(new Color(242, 207, 27, 1));
+        GameManager.Instance.setColor2(new Color(198, 106, 104, 1));
+        GameManager.Instance.setColor3(new Color(50, 94, 158, 1));
+        GameManager.Instance.setColor4(new Color(69, 121, 116, 1));
+        GameManager.Instance.setColor5(new Color(255, 255, 255, 1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,38 +1,96 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
-    public Color color1;
-    public Color color2;
-    public Color color3;
-    public Color color4;
-    public Color color5;
-    public button startButton;
+    public static GameManager Instance { get; set; }
+
+    public Color32 color1 = new Color32(255, 191, 0, 255);
+    public Color32 color2 = new Color32(232, 63, 111, 255);
+    public Color32 color3 = new Color32(34, 116, 165, 255);
+    public Color32 color4 = new Color32(50, 147, 111, 255);
+    public Color32 color5 = new Color32(255, 255, 255, 255);
+
+    public GameObject startbutton;
+    public GameObject creditsbutton;
+    public GameObject titletext;
+
+    public GameObject starttext;
+    public GameObject creditstext;
+    
+    public GameObject canvas;
+    public GameObject events;
+    public GameObject background;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(canvas);
+            DontDestroyOnLoad(events);
+        }
+        else
+        {
+            Destroy(gameObject);
+            Destroy(canvas);
+            Destroy(events);
+        }
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
-        color1 = new Color(0, 0, 0, 0);
-        color2 = new Color(0, 0, 0, 0);
-        color3 = new Color(0, 0, 0, 0);
-        color4 = new Color(0, 0, 0, 0);
-        color5 = new Color(0, 0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        starttext.GetComponent<Text>().color = color1;
+        creditstext.GetComponent<Text>().color = color1;
+        titletext.GetComponent<TextMeshProUGUI>().color = color2;
+        background.GetComponent<Image>().color = color3;
+        startbutton.GetComponent<Image>().color = color4;
+        creditsbutton.GetComponent<Image>().color = color4;
+    }
+    
+    public void setColor1(Color newcolor)
+    {
+        color1 = newcolor;
+    }
+
+    public void setColor2(Color newcolor)
+    {
+        color2 = newcolor;
+    }
+
+    public void setColor3(Color newcolor)
+    {
+        color3 = newcolor;
+    }
+
+    public void setColor4(Color newcolor)
+    {
+        color4 = newcolor;
+    }
+
+    public void setColor5(Color newcolor)
+    {
+        color5 = newcolor;
     }
 
     public void startButton()
     {
-        startButton.setActive(false);
-        StartCoroutine(LoadYourAsyncScene(Level1));
+        startbutton.SetActive(false);
+        titletext.SetActive(false);
+        StartCoroutine(LoadYourAsyncScene("Level1"));
     }
 
     IEnumerator LoadYourAsyncScene(string Scene)
